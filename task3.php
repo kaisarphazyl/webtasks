a40598e0bb787d15a8d141f918202b9794e3f19e
kaisarphazyl <43145152+kaisarphazyl@users.noreply.github.com>
2018-11-18 19:29:41 +0600

    week 10
    week 10

--- task3.php
+++ task3.php
@@ -1,72 +1,104 @@
-<html>
-<head><style>
-.cards{display:flex;flex-wrap:wrap;width:700px;margin-left:auto;margin-right:auto;}
-.cards .card{width:300px;border:1px solid red;border-radius:5px;display:flex;margin:10px;}
-.menu {display:block;width:700px;margin-left:auto;margin-right:auto;}
-.card .title{font-size:20px;font-weight:bold;}
-.card .price{color:green;}
-.card .description{display:flex;flex-direction:column;}
-.card .price{flex-grow:1;}
-.card .year{font-weight:bold;font-size:18px;}
-img {}
-</style></head>
-<body>
-
-<div class="cards">
 <?php
-$cars = [["maker"=>"Toyota","model"=>"Carina","year"=>2001,"price"=>20000,"image"=>"https://a.d-cd.net/cea52es-480.jpg"],
-["maker"=>"Toyota","model"=>"Camry","year"=>2005,"price"=>30000,"image"=>"https://d1hu588lul0tna.cloudfront.net/toyotaone/ruru/002-camry-exclusive-next-steps_tcm-3020-870114.jpg"],
-["maker"=>"Audi","model"=>"A8","year"=>1986,"price"=>12000,"image"=>"http://www.theautohost.com/_contentPages/vehicleContentPages/audi/2017/A8%20L/images/2017-Audi-A8L-exterior-grille.jpg"],
-["maker"=>"Audi","model"=>"A6","year"=>2005,"price"=>30000,"image"=>"https://www.cstatic-images.com/car-pictures/xl/USC60AUC021B021001.png"],
-["maker"=>"BMW","model"=>"X5","year"=>2007,"price"=>17000,"image"=>"https://www.bmw.ru/content/dam/bmw/marketRU/bmw_ru/all-models/x-series/x5/2015/at-a-glance/x5_at-a-glance_stagepresentation.jpg/_jcr_content/renditions/cq5dam.resized.img.1680.large.time1452864075065.jpg"],
-["maker"=>"BMW","model"=>"X5","year"=>2015,"price"=>19000,"image"=>"https://www.bmw.ru/content/dam/bmw/marketRU/bmw_ru/all-models/x-series/x5/2015/at-a-glance/x5_at-a-glance_stagepresentation.jpg/_jcr_content/renditions/cq5dam.resized.img.1680.large.time1452864075065.jpg"],
-["maker"=>"BMW","model"=>"Model 7","year"=>2014,"price"=>22000,"image"=>"https://www.bmw.ru/content/dam/bmw/marketRU/bmw_ru/all-models/x-series/x5/2015/at-a-glance/x5_at-a-glance_stagepresentation.jpg/_jcr_content/renditions/cq5dam.resized.img.1680.large.time1452864075065.jpg"],
-["maker"=>"Lada","model"=>"Granta","year"=>2017,"price"=>7000,"image"=>"http://www.kolesa.ru/uploads/2017/06/Lada-Granta-restyle-front1-1600x0-c-default.jpg"]
-];
-if (isset($_GET['maker'])) {
-    $maker = $_GET['maker'];
-    for ($i = 0; $i < sizeof($cars); $i++) {
-        $car = $cars[$i];
-        if ($car['maker'] == $maker) {
-            print '<div class="card">
-            <img src="' .$car['image'].'" style="width: 100px; height: 170px;">
-            <div class="description">
-            <p class="title">' .$car['maker']. ' ' .$car['model']. '</p>
-            <p class="price">' .$car['price']. '</p>
-            <p class="year">' .(2018 - $car['year']). ' years</p></div></div>';
-        }
-    }
-}
-if (isset($_GET['year'])) {
-    for ($i = 0; $i < sizeof($cars); $i++) {
-        $car = $cars[$i];
-        if (2018 - $car['year'] > 7) {
-            print '<div class="card">
-            <img src="' .$car['image'].'" style="width: 100px; height: 170px;">
-            <div class="description">
-            <p class="title">' .$car['maker']. ' ' .$car['model']. '</p>
-            <p class="price">' .$car['price']. '</p>
-            <p class="year">' .(2018 - $car['year']). ' years</p></div></div>';
-        }
-    }
-}
-if (isset($_GET['price'])) {
-    for ($i = 0; $i < sizeof($cars); $i++) {
-        $car = $cars[$i];
-        if ($car['price'] > 20000) {
-            print '<div class="card">
-            <img src="' .$car['image'].'" style="width: 100px; height: 170px;">
-            <div class="description">
-            <p class="title">' .$car['maker']. ' ' .$car['model']. '</p>
-            <p class="price">' .$car['price']. '</p>
-            <p class="year">' .(2018 - $car['year']). ' years</p></div></div>';
-        }
-    }
+$makers = ["Toyota","BMW","Mercedes"];
+$engine = ["gas","diesel","petroleum"];
+$cars = [["id"=>"0","maker"=>"Toyota","model"=>"Corolla","year"=>"2015","engine"=>"petroleum","price"=>"30000","additional"=>["tax","investment"]],["id"=>"1","maker"=>"BMW","model"=>"X5","year"=>"2012","engine"=>"gas","price"=>"25000","additional"=>["tax","investment","check"]],["id"=>"2","maker"=>"Toyota","model"=>"Camry","year"=>"2008","engine"=>"diesel","price"=>"35000","additional"=>["investment","check"]]];
+$selected_car = NULL;
+if (isset($_REQUEST["id"])) {
+    $selected_car = $cars[$_REQUEST["id"]];
 }
 ?>
-</div>
-<div class="menu">
-<a href="task93.php">Home</a> | <a href="task93.php?maker=Toyota">Toyota</a> | <a href="task93.php?maker=BMW">BMW</a> | <a href="task93.php?maker=Audi">Audi</a> | <a href="task93.php?maker=Lada">Lada</a></div>
-
-<div class="menu">
-<a href="task93.php?year=old">Old cars (More than 7 years)</a> | <a href="task93.php?price=expensive">Expensive cars (more than 20.000$)</a></div>
+<html>
+<head>
+<style>
+select{
+  width:200px;
+  background:white;
+  padding:5px;
+  border-radius:5px;
+  color:#444444;
+}
+input{
+  border-radius:5px;
+  padding:5px;
+}
+input[type='text'],input[type='number']{
+  width:calc(100% - 12px);
+}
+table tr td{
+  padding:3px;
+}
+</style>
+</head>
+<form action="task3.php">
+<select name="id" onchange="this.parentNode.submit()">
+    <option value="-1">Select car</option>
+    <?php 
+        for ($i = 0; $i < sizeof($cars); $i++) {?>
+    <option value="<?php print $i; ?>" <?php if(isset($_REQUEST['id']) && $_REQUEST['id'] == $i) print 'selected = "selected"';?>> <?php print $cars[$i]['maker'] . ' ' . $cars[$i]['model']; ?> </option>
+        <?php } ?>
+</select>
+</form>
+<form>
+    <table>
+        <tr>
+            <td>Maker:</td>
+            <td>
+                <select name="maker" id="maker" value="<?php if ($selected_car != NULL) print $selected_car['maker']?>">
+                    <?php 
+                    foreach ($makers as $maker) {
+                        ?>
+                        <option value="<?= $maker ?>" <?php if(isset($_REQUEST['id']) && $maker == $selected_car['maker'])print 'selected="selected"' ?> ><?= $maker?></option>;
+                        <?php
+                    }
+                    ?>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td>Year: </td>
+            <td>
+            <select name="year" id="year" value="<?php if ($selected_car != NULL) print $selected_car['year']?>">
+                    <?php 
+                    for ($i = 2018; $i >= 1980; $i--) {
+                        ?>
+                        <option value="<?= $i ?>" <?php if(isset($_REQUEST['id']) && $i == $selected_car['year'])print 'selected="selected"' ?>><?= $i ?></option>;
+                        <?php
+                    }
+                    ?>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td>Model:</td>
+            <td><input type="text" name="model" value="<?php if ($selected_car != NULL) print $selected_car['model']?>"></td>
+        </tr>
+        <tr>
+            <td>Engine:</td>
+            <td>
+                <?php 
+                    foreach ($engine as $eng) {
+                        ?>
+                        <input type="radio" name="engine" value = "<?= $eng ?>" <?php if ($selected_car != NULL && $eng == $selected_car['engine']) print "checked"; ?>>
+                        <?=  $eng ?>
+                        <?php
+                    }
+                ?>
+            </td>
+        </tr>
+        <tr>
+        <tr>
+            <td>Price:</td>
+            <td><input type="text" name="price" value="<?php if ($selected_car != NULL) print $selected_car['price']?>"></td>
+        </tr> 
+        </tr>
+        <tr>
+            <td>Additional: </td>
+            <td>  
+                <input type="checkbox" name="tax_payed" <?php if ($selected_car != null && in_array('tax', $selected_car['additional'])) print "checked"; ?>> tax payed <br>
+                <input type="checkbox" name="technical" <?php if ($selected_car != null && in_array('check', $selected_car['additional'])) print "checked"; ?>> technical check passed <br>
+                <input type="checkbox" name="invest" <?php if ($selected_car != null && in_array('investment', $selected_car['additional'])) print "checked"; ?>> doesn't require investment <br>
+            </td>
+        </tr>
+    </table>
+    <input type="submit"/>
+</form>
